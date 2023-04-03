ead370f73af8026be18a189a394bd08bcd9537a8
Emre Kılıç <emre1kilic@outlook.com>
2023-04-04 02:19:26 +0300

    define methods

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import psutil
 
@@ -5,10 +6,17 @@ import psutil
 print("Welcome to Interapi")
 print("CPU usage: ",  psutil.cpu_times())
 
+# todo get cpu, disk, memory, network usage
+# todo post to the specified API endpoint
+
+
+def get_sys_config():
+    print(f"System: {sys.platform} ")
+
 
 def get_cpu_percent(user_range, user_interval):
     for i in range(user_range):
         print(psutil.cpu_percent(interval=user_interval))
-        
-        
-print(get_cpu_percent(5,1))
+
+
+print(get_cpu_percent(5, 1))
